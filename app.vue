0062dbe220a5c8f89358ee92ad952632ff4c361a
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-11-18 20:42:15 -0300

    1. Construyendo la interfaz Gráfica

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
+  <Html class="bg-emerald-500">
   <div>
     <NuxtPage/>
   </div>
+  </Html>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/css/vello.css'],
   compatibilityDate: '2024-04-03',
   devtools: {enabled: true},
   modules: ['@pinia/nuxt', '@nuxt/ui'],
   colorMode: {
     preference: 'light',
-  }
+  },
 })
